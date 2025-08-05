84ed026c11298bbe88dd536da049ce693fd093e0
Elisa Rubin <elisa.rubin@otto.to.it>
2025-08-05 12:29:27 +0200

    feat: allow field tools configuration

--- example/blog/models.py
+++ example/blog/models.py
@@ -3,7 +3,25 @@ from editor_js.fields import EditorJSField
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    body = EditorJSField()
+
+    # This field will only have Header and List tools
+    summary = EditorJSField(
+        blank=True,
+        # Specific tools configuration for this field
+        tools={
+            'header': {
+                'class': 'Header',
+                'script': 'https://cdn.jsdelivr.net/npm/@editorjs/header@latest',
+            },
+            'list': {
+                'class': 'EditorjsList',
+                'script': 'https://cdn.jsdelivr.net/npm/@editorjs/list@latest',
+            }
+        }
+    )
+
+    # This field will use the default or global tool configuration
+    body = EditorJSField(blank=True)
 
     def __str__(self):
         return self.title
